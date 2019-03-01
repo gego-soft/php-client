e966006c93bba113952784afb7b2cf4e30990d02
Karthick <karthick@gegosoft.com>
2019-03-01 13:41:27 +0530

    update

--- lib/BlockCypher/Crypto/PrivateKey.php
+++ lib/BlockCypher/Crypto/PrivateKey.php
@@ -2,8 +2,8 @@
 
 namespace BlockCypher\Crypto;
 
-use BitWasp\Bitcoin\Key\PrivateKeyInterface;
-
+//use BitWasp\Bitcoin\Key\PrivateKeyInterface;
+use BitWasp\Bitcoin\Crypto\EcAdapter\Key\PrivateKeyInterface;
 /**
  * Class PrivateKey
  * @package BlockCypher\Crypto
